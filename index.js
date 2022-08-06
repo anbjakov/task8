ffb239f4fdacfa8da402bb83b432fe491d96e27b
Andrii Biakov <an.bjakov@gmail.com>
2022-08-06 20:30:41 +0300

    added sumTwoSmallestNumbers

--- index.js
+++ index.js
@@ -33,3 +33,16 @@ console.log(calc.mult(10)); // 150
 console.log(calc.sub(40)); // 110
 console.log(calc.div(10)); // 11
 
+console.log('--------task 2 -----------------------')
+
+let sumTwoSmallestNumbers = function(args){
+    let argArray =  [...arguments].sort( (a,b)=>a-b )
+    if (argArray.length<2) {
+        console.error('input must have at least 2 values!');
+        return;
+    }
+    return argArray[0] + argArray[1]
+}
+console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77)); // 7
+console.log(sumTwoSmallestNumbers(1, 3, 2)); // 3
+console.log(sumTwoSmallestNumbers(1)); // undefined и отдельный console.error("...")
