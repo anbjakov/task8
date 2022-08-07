f2bd8e618b67a6c9cbdac144f872621a28737534
Andrii Biakov <an.bjakov@gmail.com>
2022-08-07 17:33:14 +0300

    new solution for calc, edit check for task 2

--- index.js
+++ index.js
@@ -2,45 +2,63 @@
 
 function createCalculator (initValue){
     let calcControl = {
-        setValue: 'initValue',
+        memoryValue: 'initValue',
         readMemory: function() {
-            return this.setValue
+            return this.memoryValue
         },
         sum: function(increaseValue){
-            this.setValue = this.readMemory()+increaseValue;
+            this.memoryValue = this.readMemory()+increaseValue;
             return this.readMemory() ;
         },
         mult: function(multValue){
-            this.setValue = this.readMemory() * multValue;
+            this.memoryValue = this.readMemory() * multValue;
             return this.readMemory() ;
         },
         sub:  function(subValue){
-            this.setValue = this.readMemory() - subValue;
+            this.memoryValue = this.readMemory() - subValue;
             return this.readMemory();
         },
         div:  function(divValue){
-            this.setValue = this.readMemory() /divValue;
+            this.memoryValue = this.readMemory() /divValue;
             return this.readMemory();
         },
     }
-    calcControl.setValue = initValue
+    calcControl.memoryValue = initValue
     return calcControl
 }
 let calc  = createCalculator(10);
-console.log(calc .sum(5)); /// 15
-console.log(calc .mult(10)); // 150
-console.log(calc .sub(40)); // 110
-console.log(calc .div(10)); // 11
+console.log(calc.sum(5)); /// 15
+console.log(calc.mult(10)); // 150
+console.log(calc.sub(40)); // 110
+console.log(calc.div(10)); // 11
 
+console.log('------- new function calc-------------');
 
-console.log('--------task 2 -----------------------')
+function simpleCalcSolution (initResult) {
+    let result = initResult;
+    let calcControls = {
+        sum: (sumValue)=>{result = result + sumValue; return result},
+        mult: (multValue)=>{result = result * multValue; return result},
+        sub: (subValue)=>{result = result - subValue; return result},
+        div: (divValue)=>{result = result / divValue; return result}
+    }
+    return calcControls
+}
+
+let boringСalc = simpleCalcSolution(10);
+console.log(boringСalc.sum(5)); /// 15
+console.log(boringСalc.mult(10)); // 150
+console.log(boringСalc.sub(40)); // 110
+console.log(boringСalc.div(10)); // 11
+
+console.log('--------task 2 -----------------------');
 
 let sumTwoSmallestNumbers = function(args){
-    let argArray =  [...arguments].sort( (a,b)=>a-b )
-    if (argArray.length<2) {
+    if ([...arguments].length<2) {
         console.error('input must have at least 2 values!');
         return;
     }
+    let argArray =  [...arguments].sort( (a,b)=>a-b );
     return argArray[0] + argArray[1]
 }
 console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77)); // 7
